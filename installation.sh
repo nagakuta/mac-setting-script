c144961143e5e40d79991ec5753e16cf5fbe8014
nagakuta <nagakuta0314@gmail.com>
2017-07-08 18:54:17 +0900

    First Commit

---
+++ installation.sh
@@ -0,0 +1,17 @@
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder PathBarRootAtHome -bool yes
+defaults write com.apple.Finder QuitMenuItem -bool true
+defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+defaults write -g NSNavRecentPlacesLimit -int 10
+killall Finder
+
+defaults write com.apple.dock static-only -boolean true
+defaults write com.apple.dashboard mcx-disabled -bool true
+killall Dock
+
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture name ""
+defaults write com.apple.screencapture location ~/Pictures/Screenshots/
+killall SystemUIServer
+
