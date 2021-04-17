5f7e4a4a4dc5f0e135bfd2f2b05eb1a13868d629
nagakuta <nagakuta0314+github@gmail.com>
2021-04-18 02:56:47 +0900

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,10 +3,12 @@ defaults write com.apple.finder PathBarRootAtHome -bool true
 defaults write com.apple.Finder QuitMenuItem -bool true
 defaults write com.apple.finder QLEnableTextSelection -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+defaults write com.apple.finder QLEnableTextSelection -bool true
 killall Finder
 
 defaults write com.apple.dock static-only -bool true
 defaults write com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.Dock showhidden -bool true
 killall Dock
 
 defaults write com.apple.screencapture location $HOME/Pictures/Screenshots/
