8ef2419d830691056c457bd2d572e532be90ba8e
nagakuta <nagakuta0314+github@gmail.com>
2021-05-06 15:48:25 +0900

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -4,6 +4,7 @@ defaults write com.apple.Finder QuitMenuItem -bool true
 defaults write com.apple.finder QLEnableTextSelection -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write com.apple.screencapture disable-shadow -bool true
 killall Finder
 
 defaults write com.apple.dock static-only -bool true
@@ -11,5 +12,5 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 defaults write com.apple.Dock showhidden -bool true
 killall Dock
 
-defaults write com.apple.screencapture location $HOME/Pictures/Screenshots/
+mkdir -p $HOME/Pictures/Screenshots && defaults write com.apple.screencapture location $HOME/Pictures/Screenshots/
 killall SystemUIServer
